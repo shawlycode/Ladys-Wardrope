3af184da44c1cd245a91ac4135649dd0eca32c71
Shawlycode <kutinaorlando@gmail.com>
2022-04-15 19:52:28 +0000

    added dummy data and assets

---
+++ src/Dummy_Data/Data.js
@@ -0,0 +1,44 @@
+export const electronics = [
+  {
+    id: 1,
+    name: "Derick-Lam",
+    image: require("../../assets/dericl-lamp.jpeg"),
+    price: 299.99,
+  },
+  {
+    id: 2,
+    name: " Echo",
+    image: require("../../assets/dericl-lamp.jpeg"),
+    price: 199,
+  },
+  {
+    id: 3,
+    name: "Roses",
+    image: require("../../assets/look.jpeg"),
+    price: 300,
+  },
+  {
+    id: 4,
+    name: "Sleek Black",
+    image: require("../../assets/img1.jpg"),
+    price: 300,
+  },
+];
+
+export const books = [
+  {
+    id: 4,
+    name: "How to Kill a Mocking Bird",
+    price: 10,
+  },
+  {
+    id: 5,
+    name: "War of Art",
+    price: 7,
+  },
+  {
+    id: 6,
+    name: "Relentless",
+    price: 5.99,
+  },
+];
